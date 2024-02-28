2e632e350912ec33f23beedeb96101388147771d
svitalis123 <vitalis.wilson@yahoo.com>
2024-02-28 21:28:26 +0300

    changed password to passoword_digets and returning error for creating functionality as well as success messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,15 +16,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      render json: { user: @user, message: 'User was successfully created' }, status: :created
     else
-      render :new
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :role, :country_id)
+    params.require(:user).permit(:name, :email, :password_digest, :role, :country_id)
   end
 end
