8ae0f7d0cd96ff104b9007e102dbda0aedb744fc
svitalis123 <vitalis.wilson@yahoo.com>
2024-04-01 20:15:08 +0300

    changed migrations

--- db/migrate/20240225142624_add_country_ref_to_properties.rb
+++
@@ -1,5 +0,0 @@
-class AddCountryRefToProperties < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :properties, :country, null: false, foreign_key: true
-  end
-end
--- db/migrate/20240225143720_add_country_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCountryIdToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :users, :country, null: false, foreign_key: true
-  end
-end
