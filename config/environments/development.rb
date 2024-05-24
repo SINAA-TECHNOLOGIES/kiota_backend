8d085863c16c42ad5fcf2e36aa8cef433858e74b
svitalis123 <vitalis.wilson@yahoo.com>
2024-05-24 10:36:44 +0300

    changed stuctured data

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,8 +56,11 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # before
+  # config.hosts << ENV['DEVELOPMENT_HOSTS']
+  # after
+  config.hosts << "kiota-api.onrender.com"
 
-  config.hosts << ENV['DEVELOPMENT_HOSTS']
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
