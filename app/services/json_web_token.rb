267d0a9fb6d0ac4917646191a2b4240c9974d9e6
svitalis123 <vitalis.wilson@yahoo.com>
2024-05-28 21:30:35 +0300

    debug2

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -1,15 +1,43 @@
+# class JsonWebToken
+#   class << self
+#     def encode(payload, exp = 24.hours.from_now)
+#       payload[:exp] = exp.to_i
+#       JWT.encode(payload, Rails.application.secrets.secret_key_base)
+#     end
+
+#     def decode(token)
+#       decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+#       HashWithIndifferentAccess.new decoded
+#     rescue JWT::ExpiredSignature, JWT::DecodeError
+#       nil
+#     end
+#   end
+# end
 class JsonWebToken
   class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      token = JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      Rails.logger.info "Generated Token: #{token}"
+      token
     end
 
     def decode(token)
-      decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-      HashWithIndifferentAccess.new decoded
-    rescue JWT::ExpiredSignature, JWT::DecodeError
-      nil
+      Rails.logger.info "Decoding Token: #{token}"
+      begin
+        decoded = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+        Rails.logger.info "Decoded JWT Body: #{decoded.inspect}"
+        HashWithIndifferentAccess.new(decoded)
+      rescue JWT::ExpiredSignature
+        Rails.logger.error "JWT Decode Error: Token has expired"
+        nil
+      rescue JWT::VerificationError
+        Rails.logger.error "JWT Decode Error: Verification failed"
+        nil
+      rescue JWT::DecodeError
+        Rails.logger.error "JWT Decode Error: Decode error"
+        nil
+      end
     end
   end
 end
